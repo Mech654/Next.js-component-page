6400cc6c61cd4063aa67f681d63287c820c2020b
Mech654 <drahmet2007@hotmail.com>
2025-01-28 15:38:21 +0100

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Header from './components/header'; // Relative path if both are under 'ap
 export default function Home() {
   return (
     <div className="min-h-screen bg-gradient-to-r from-gray-800 via-gray-700 to-gray-900 ">
-      <Header("wadadwa")/>
+      <Header()/>
     </div>
   );
 }
