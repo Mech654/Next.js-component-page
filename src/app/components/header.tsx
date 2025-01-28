95b81d78a6d2bab575b41207aa53ea9cda00f8e3
Mech654 <drahmet2007@hotmail.com>
2025-01-28 15:32:53 +0100

    update
    lets start chat. time to cook or get cooked

---
+++ src/app/components/header.tsx
@@ -0,0 +1,9 @@
+const Header = (headertext) => {
+    return (
+        <header className="opacity-25 bg-gradient-to-r from-cyan-500 to-blue-500 bg-opacity-50 backdrop-blur-md w-full h-20">
+            <h1 className="opacity-100 text-white text-2xl font-bold text-center" >Welcome, User47563256</h1>
+        </header>
+    );
+}
+
+export default Header; 
